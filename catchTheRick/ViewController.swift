e3233dc0e7791bd30ff9db4eb9c1233f07a0fa6c
fahriye zeynep çakır <96346390+zeynepfahriye@users.noreply.github.com>
2022-08-22 00:14:08 +0300

    Initial Commit

---
+++ catchTheRick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  catchTheRick
+//
+//  Created by Fahriye Zeynep Çakır on 22.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ catchTheRickUITests/catchTheRickUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  catchTheRickUITestsLaunchTests.swift
+//  catchTheRickUITests
+//
+//  Created by Fahriye Zeynep Çakır on 22.08.2022.
+//
+
+import XCTest
+
+class catchTheRickUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
